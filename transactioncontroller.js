fa996ee38dffb3b1f7d4c07ceb9c945214a2fc89
hugebing <jbjyuan@gmail.com>
2023-01-29 12:59:13 +0800

    change queue type and modify retransmission delay

--- transactioncontroller.js
+++ transactioncontroller.js
@@ -12,7 +12,7 @@ const logOfResult = new Console({
     stdout: fs.createWriteStream(`${timeOfFile}_logOfResult`),
 });
 
-var oneByOneQueue = [];
+var oneByOneQueue = {};
 var amountOfPendingTransaction = 0;
 var amountOfErrorOfTransaction = 0;
 var amountOfCompleteTransaction = 0;
@@ -24,7 +24,8 @@ var timeIntervalOfMinusUpperOfPendingTransaction = 5000;
 var amountOfMvccrc = 0;
 var ratioOfFuncOfMvccrc = {};
 var amountOfFuncOfNormalTransaction = {};
-var waitingTimeOfResend = {};
+let countOfExorbitantRPS = 0;
+let countOfError = 0;
 
 var preTime = 0; 
 var amountOfCompleteTransactionOfPreTime = 0
@@ -51,7 +52,11 @@ async function addNormalQueue(transaction, needResned, ...args) {
 
 async function addOneByOneQueue (transaction, needResned,  ...args) {
     return new Promise((res, rej) => {
-        oneByOneQueue.push([transaction, needResned, res, args]);
+        let key = `${transaction.contract.chaincodeId}_${transaction.name}`;
+        if (oneByOneQueue[key] == undefined){
+            oneByOneQueue[key] = [];
+        }
+        oneByOneQueue[key].push([transaction, needResned, res, args]);
     })
 }
 
@@ -79,37 +84,42 @@ exports.normalController = async () => {;
 }
 
 exports.oneByOneController = async () => {
-    for(let i=0;i<oneByOneQueue.length;i++){
-        let chaincodeIdPlusNameFuncOfMvccrc = `${oneByOneQueue[i][0].contract.chaincodeId}_${oneByOneQueue[i][0].name}`;
-        if (oneByOneLock[`${chaincodeIdPlusNameFuncOfMvccrc}`] != true) { 
-            let transaction = oneByOneQueue[i][0];
-            let needResned = oneByOneQueue[i][1];
-            let callback = oneByOneQueue[i][2];
-            let args = oneByOneQueue[i][3];
-            oneByOneLock[`${transaction.contract.chaincodeId}_${transaction.name}`] = true;
-            oneByOneQueue.splice(i, 1); 
+    for (var key in oneByOneQueue){
+        if (oneByOneLock[key] != true) { 
+            let transaction = oneByOneQueue[key][0][0];
+            let needResned = oneByOneQueue[key][0][1];
+            let callback = oneByOneQueue[key][0][2];
+            let args = oneByOneQueue[key][0][3];
+            oneByOneLock[key] = true;
+            oneByOneQueue[key].splice(0, 1);
+            if (!oneByOneQueue[key].length) {
+                delete oneByOneQueue[key];
+            }
+            console.log(oneByOneQueue);
             addPendingTransaction();
             const result = await submitTransaction(transaction, needResned, args);
-            oneByOneLock[`${transaction.contract.chaincodeId}_${transaction.name}`] = false;
+            oneByOneLock[key] = false;
             callback(result);
         }
     }
 }
 
 async function transactionResend(transaction, needResned, args, countOfResned){
-    // let resendTime = waitTime(transaction.getName()) * 1000;
-    let resendTime = 0;
-    console.log(`Trsanction ID ${transaction.getTransactionId()} MVCC_READ_CONFLICT "RESEND" WAIT ${resendTime}`);
-    await wait(resendTime);
-    let newTransaction = await createTransactionAndGetData(transaction.contract, transaction.name);
     countOfResned += 1;
-    await submitTransaction(newTransaction, needResned, args, countOfResned).then((res)=>{
-        return res;
+    let time = 0
+    if(countOfResned <= 4){
+        time = Math.pow(2, countOfResned);
+    } else {
+        time = 16;
+    }
+    console.log(`Trsanction ID ${transaction.getTransactionId()} MVCC_READ_CONFLICT "RESEND" WAIT ${time}`);
+    await wait(Math.floor(Math.random() * time * 1000));
+    let newTransaction = await createTransactionAndGetData(transaction.contract, transaction.name);
+    console.log(countOfResned);
+    return await submitTransaction(newTransaction, needResned, args, countOfResned).then((res)=>{
+        return [newTransaction.getTransactionId(), res, countOfResned];
     });
 }
-
-let countOfExorbitantRPS = 0;
-let countOfError = 0;
 
 async function submitTransaction(transaction, needResned, args, countOfResned=0){
     let startTime = Date.now();
@@ -210,7 +220,7 @@ exports.controllerParameterModify = async () => {
         "tpsOfMvccrc": tpsOfMvccrc,
         "ratioOfMvccrc": tpsOfMvccrc / (tps + tpsOfMvccrc),
         "lengthOfNormalQueue": normalQueue.length,
-        "lengthOfOneByOneQueue": oneByOneQueue.length,
+        "amountOfTypeOfOneByOneQueue": Object.keys(oneByOneQueue).length,
         "amountOfCompleteTransaction": amountOfCompleteTransactionOfNow,
         "amountOfMvccrcOfNow": amountOfMvccrcOfNow,
         "countOfExorbitantRPS": countOfExorbitantRPS,
@@ -219,40 +229,6 @@ exports.controllerParameterModify = async () => {
     
     console.log(result);
     logOfResult.log(result);
-
-    // 要不要增加上限?
-    let amountOfFuncOfMvccrc = Object.keys(ratioOfFuncOfMvccrc);
-
-    // 取重送延遲 Function 與有發生 MVCCRC Function 的差集
-    let diffOfArray = Object.keys(waitingTimeOfResend).filter((e)=>{return amountOfFuncOfMvccrc.indexOf(e) === -1})
-
-    for(let i=0;i<diffOfArray.length;i++){
-        waitingTimeOfResend[diffOfArray[i]] = waitingTimeOfResend[diffOfArray[i]] - 1;
-        if(waitingTimeOfResend[diffOfArray[i]]==0){
-            delete waitingTimeOfResend[diffOfArray[i]];
-        }
-    }
-
-    for(let i=0;i<amountOfFuncOfMvccrc.length;i++){
-        if(waitingTimeOfResend[amountOfFuncOfMvccrc[i]]==undefined){
-            waitingTimeOfResend[amountOfFuncOfMvccrc[i]] = 0;
-        }
-
-        if(amountOfFuncOfNormalTransaction[amountOfFuncOfMvccrc[i]]==undefined  && waitingTimeOfResend[amountOfFuncOfMvccrc[i]] <= 15){
-            waitingTimeOfResend[amountOfFuncOfMvccrc[i]] += 1;
-        } else if((ratioOfFuncOfMvccrc[amountOfFuncOfMvccrc[i]]/(ratioOfFuncOfMvccrc[amountOfFuncOfMvccrc[i]]+amountOfFuncOfNormalTransaction[amountOfFuncOfMvccrc[i]])) > 0.5 && waitingTimeOfResend[amountOfFuncOfMvccrc[i]] <= 15){
-            waitingTimeOfResend[amountOfFuncOfMvccrc[i]] += 1;
-        } else if((ratioOfFuncOfMvccrc[amountOfFuncOfMvccrc[i]]/(ratioOfFuncOfMvccrc[amountOfFuncOfMvccrc[i]]+amountOfFuncOfNormalTransaction[amountOfFuncOfMvccrc[i]])) < 0.1) {
-            waitingTimeOfResend[amountOfFuncOfMvccrc[i]] = waitingTimeOfResend[amountOfFuncOfMvccrc[i]] -= 1;
-        }
-
-        if(waitingTimeOfResend[amountOfFuncOfMvccrc[i]]==0){
-            delete waitingTimeOfResend[amountOfFuncOfMvccrc[i]];
-        }
-    }
-
-    amountOfFuncOfNormalTransaction = {};
-    ratioOfFuncOfMvccrc = {};
 
     if(amountOfErrorOfTransaction >= 1 && nowTime - lastTimeOfMinusUpperOfPendingTransaction >= timeIntervalOfMinusUpperOfPendingTransaction){
         lastTimeOfMinusUpperOfPendingTransaction = nowTime;
